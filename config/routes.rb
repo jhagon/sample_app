f7e8275225bca273276419abf1b442a2fa0ea509
J.P.Hagon <njph@home-PC.(none)>
2011-04-20 20:21:38 +0100

    Optimized Static Page Layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  root  to: => 'pages#home'
+  root  :to => 'pages#home'
   get "pages/home"
 
   get "pages/contact"
