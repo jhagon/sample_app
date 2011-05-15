fae347b265d474b0121c4f26e866ad5801b89a21
J.P.Hagon <njph@home-PC.(none)>
2011-05-15 15:01:53 +0100

    Done with sign in

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -20,9 +20,9 @@ describe "Users" do
       end
     end
 
-    describe "failure" do
+    describe "success" do
 
-      it "should not make a new user" do
+      it "should make a new user" do
         lambda do
           visit signup_path
           fill_in "Name",         :with => "Example User"
@@ -35,6 +35,32 @@ describe "Users" do
                                         :content => "Welcome")
           response.should render_template('users/show')
         end.should change(User, :count).by(1)
+      end
+    end
+  end
+
+  describe "sign in/out" do
+
+    describe "failure" do
+      it "should not sign a user in" do
+        visit signin_path
+        fill_in "Email",    :with => ""
+        fill_in "Password", :with => ""
+        click_button
+        response.should have_selector("div.flash.error", :content => "Invalid")
+      end
+    end
+
+    describe "success" do
+      it "should sign a user in and out" do
+        user = Factory(:user)
+        visit signin_path
+        fill_in "Email",    :with => user.email
+        fill_in "Password", :with => user.password
+        click_button
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
       end
     end
   end
